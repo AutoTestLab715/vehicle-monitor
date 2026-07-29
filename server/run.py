fcd508772381ad2a5f09a4e5c326f4de54995d97
AutoTestLab715 <2654104506@qq.com>
2026-07-29 10:40:36 +0800

    fix run.py indentation

--- server/run.py
+++ server/run.py
@@ -2,14 +2,17 @@ import os
 
 from app import create_app
 
+
 app, socketio = create_app()
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', '3000'))
-   socketio.run(
-    app,
-    host='0.0.0.0',
-    port=port,
-    debug=False,
-    allow_unsafe_werkzeug=True
-)
+
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=port,
+        debug=False,
+        allow_unsafe_werkzeug=True
+    )
