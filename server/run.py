166d60b7af7cfdf5210b0c53368b5e8ab31dd576
AutoTestLab715 <2654104506@qq.com>
2026-07-29 10:33:16 +0800

    fix flask socketio github actions

--- server/run.py
+++ server/run.py
@@ -6,4 +6,10 @@ app, socketio = create_app()
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', '3000'))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+   socketio.run(
+    app,
+    host='0.0.0.0',
+    port=port,
+    debug=False,
+    allow_unsafe_werkzeug=True
+)
